63e9c6c7c442611007dacd9ac5ab27f04086bed4
Licanueto <licanueto@hotmail.com>
2018-10-02 09:33:37 -0300

    checkea que la key=1 exista en el array $base antes de crear la constante BASE

--- config/constantes.php
+++ config/constantes.php
@@ -6,13 +6,9 @@ define('ROOT', str_replace('\\','/',dirname(__DIR__) . "/"));
 //define('ROOT', str_replace('localhost/tpbecharge/'));
 
 $base=explode($_SERVER['DOCUMENT_ROOT'],ROOT);
-  define("BASE",$base[1]);
-
+  if(array_key_exists(1, $base)) define("BASE",$base[1]);
   define('HOST',"localhost");
   define('USER',"root");
   define('PASS',"");
   define('DB',"beerghost");
-
-
-
 ?>
